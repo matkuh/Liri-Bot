5584f4647f44a19c14ec726400ce07feb2c2ff87
matkuh <matkuh@gmail.com>
2019-06-20 16:48:12 -0700

    add concert-this function to search a song name on spotify

--- liri.js
+++ liri.js
@@ -1,21 +1,54 @@
+var axios = require('axios');
+
+var fs = require("fs")
+
 require("dotenv").config();
 
 var keys = require("./keys");
 
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
 
-var concertqueryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+var spotify = new Spotify(keys.spotify);
 
-var search = processargv[2];
+var search = process.argv[2];
 
-var userInput = process.argv[3];
+var userInput = process.argv.slice(3).join(" ");
 
-var axios = require("axios");
-
-if (search === "concert-this"){
-    axios.get(concertqueryURL).then(
-  function(response) {
-    console.log(response);
-  })
-
+function concertThis() {
+  var URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  var artist = userInput
+  axios.get(URL).then(
+    function (response) {
+      var jsonData = response.data[0];
+      var showData = [
+        "Name: " + jsonData.venue.name,
+        "Venue Location: " + jsonData.venue.city,
+        "Date: " + jsonData.datetime,
+      ].join("\n\n");
+      console.log(showData)
+    })
 }
+function spotifyThis() {
+  var songname = userInput
+  spotify.search({ type: 'track', query: songname }, function (err, response) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    var showData = [
+      "Artist Name: " + response.tracks.items[0].artists[0].name,
+      "Song Name: " + response.tracks.items[0].name,
+      "Link: " + response.tracks.items[0].preview_url,
+      "Album: " + response.tracks.items[0].album.name,
+    ].join("\n\n");
+    console.log(showData)
+  })
+}
+
+if (search === "spotify-this-song") {
+  spotifyThis();
+}
+if (search === "concert-this") {
+  concertThis();
+}
+
+
